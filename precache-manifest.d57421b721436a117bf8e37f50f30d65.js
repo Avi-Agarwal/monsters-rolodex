8581f9b7417d4dbfbcea467a3ff678f524b72f07
Avi Agarwal <avi7agarwal@gmail.com>
2020-06-21 21:41:40 -0700

    Updates

---
+++ precache-manifest.d57421b721436a117bf8e37f50f30d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d6c53bc63623c59c7de8edc9ee0944",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b968a3f6759d87e92796",
+    "url": "/monsters-rolodex/static/css/main.49699e9a.chunk.css"
+  },
+  {
+    "revision": "4566aafd3657507647d5",
+    "url": "/monsters-rolodex/static/js/2.85bd22ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.85bd22ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b968a3f6759d87e92796",
+    "url": "/monsters-rolodex/static/js/main.1794c446.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
